e407508cd6df00e406734e2f6763927996750938
mauricio076@gmail.com <Fender076>
2019-01-06 23:14:26 +0100

    Single page application Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,63 +1,74 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from "react-router-dom";
 
-    function RenderDish({dish}){
+function RenderDish({dish}) {
+    return (
+        <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name}/>
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    )
+}
+
+function RenderComments({comments}) {
+    if (!comments || comments.length === 0) {
+        return (<div></div>);
+    } else {
+        var options = {year: 'numeric', month: 'short', day: 'numeric'};
+        let list = comments.map((comment) => {
+            return (
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p> -- {comment.author}, {new Date(comment.date).toLocaleDateString("en-US", options)} </p>
+                </li>
+            )
+        });
+
         return (
-            <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <div>
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {list}
+                </ul>
+            </div>
         )
     }
+}
 
-   function RenderComments({comments}) {
-        if (!comments || comments.length === 0) {
-            return (<div></div>);
-        } else {
-            var options = {year: 'numeric', month: 'short', day: 'numeric'};
-            let list = comments.map((comment) => {
-                return (
-                    <li key={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p> -- {comment.author}, {new Date(comment.date).toLocaleDateString("en-US", options)} </p>
-                    </li>
-                )
-            });
-
-            return (
-                <div>
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        {list}
-                    </ul>
+const DishDetail = (props) => {
+    if (props.dish != null) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem> <Link to='/home'>Home</Link> </BreadcrumbItem>
+                        <BreadcrumbItem> <Link to='/menu'>Menu</Link> </BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr/>
+                    </div>
                 </div>
-            )
-        }
-    }
-
-    const DishDetail = (props) => {
-        const dish = props.dish;
-        if (dish != null) {
-            return (
-                <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
                         <RenderDish dish={props.dish}/>
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.dish.comments} />
+                        <RenderComments comments={props.comments}/>
                     </div>
                 </div>
-                </div>
-            )
-        } else {
-            return (
-                <div></div>
-            )
-        }
+            </div>
+        )
+    } else {
+        return (
+            <div></div>
+        )
+    }
 
 };
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
@@ -13,7 +13,7 @@ import {Switch, Route, Redirect} from "react-router-dom";
 
 class Main extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -27,14 +27,22 @@ class Main extends Component {
 
     render() {
 
-        const  HomePage = () => {
+        const HomePage = () => {
             return (
                 <Home
                     dish={this.state.dishes.filter((dish) => dish.featured)[0]}
                     promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
                     leader={this.state.leaders.filter((leader) => leader.featured)[0]}
                 />
-                );
+            );
+        };
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                />
+
+            );
         };
 
         return (
@@ -42,9 +50,10 @@ class Main extends Component {
                 <Header/>
                 <Switch>
                     <Route path="/home" component={HomePage}/>
-                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+                    <Route path="/menu/:dishId" component={DishWithId}/>
                     <Route exact path="/contactus" component={Contact}/>
-                    <Redirect to="/home" />
+                    <Redirect to="/home"/>
                 </Switch>
                 <Footer/>
             </>
