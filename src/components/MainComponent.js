9b23dd86c4b5aa045d15432598f71a964195b521
mauricio076@gmail.com <Fender076>
2019-01-07 06:27:00 +0100

    Assigment 2: React Router and Single Page Applications

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -9,7 +10,7 @@ import {DISHES} from "../shared/dishes";
 import {COMMENTS} from "../shared/comments";
 import {LEADERS} from "../shared/leaders";
 import {PROMOTIONS} from "../shared/promotions";
-import {Switch, Route, Redirect} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 
 class Main extends Component {
 
@@ -53,6 +54,7 @@ class Main extends Component {
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
                     <Route path="/menu/:dishId" component={DishWithId}/>
                     <Route exact path="/contactus" component={Contact}/>
+                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}/>
                     <Redirect to="/home"/>
                 </Switch>
                 <Footer/>
